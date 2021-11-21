f6a2ef210d318f5ed33e3cc1165b4e6eb523bb0d
Anirudh Aswal <aswalanirudh@outlook.in>
2021-11-21 21:27:05 +0530

    Added news place holder

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,95 +1,109 @@
 <template>
   <q-layout view="lHr lpR lFr">
-
     <q-header bordered class="bg-white text-black">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title class = "text-weight-bold">
-          <q-avatar>
-            
-          </q-avatar>
-          <span class = "gt-sm"> {{$route.name}} </span>
-          <q-icon name="fas fa-brain" size="sm" color="primary" class = "q-pa-md lt-md absolute-center"/>
+        <q-toolbar-title class="text-weight-bold">
+          <q-avatar> </q-avatar>
+          <span class="gt-sm"> {{ $route.name }} </span>
+          <q-icon
+            name="fas fa-brain"
+            size="sm"
+            color="primary"
+            class="q-pa-md lt-md absolute-center"
+          />
         </q-toolbar-title>
-
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered :width="250">
-      <q-icon name="fas fa-brain" size="lg" color="primary" class = "q-pa-md"/>
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      :width="250"
+    >
+      <q-icon name="fas fa-brain" size="lg" color="primary" class="q-pa-md" />
       <q-list bordered padding>
-      <q-item>
-        <q-item-section>
-          <q-item-label overline>Acess Sero tools</q-item-label>
-          <q-item-label>Easily acess our tools</q-item-label>
-          <q-item-label caption>Find therapists, Account management</q-item-label>
-        </q-item-section>
+        <q-item>
+          <q-item-section>
+            <q-item-label overline>Acess Sero tools</q-item-label>
+            <q-item-label>Easily acess our tools</q-item-label>
+            <q-item-label caption
+              >Find therapists, Account management</q-item-label
+            >
+          </q-item-section>
+        </q-item>
 
-      </q-item>
+        <q-separator spaced />
+        <q-item-label header>Menu</q-item-label>
 
-      <q-separator spaced />
-      <q-item-label header>Menu</q-item-label>
+        <q-item to="/" v-ripple clickable exact>
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section class="text-weight-lg">Home</q-item-section>
+        </q-item>
 
-      <q-item
-      to="/"
-      v-ripple
-      clickable
-      exact>
-        <q-item-section avatar>
-          <q-icon name="home" />
-        </q-item-section>
-        <q-item-section class="text-weight-lg">Home</q-item-section>
-      </q-item>
+        <q-item to="find" v-ripple clickable exact>
+          <q-item-section avatar>
+            <q-icon name="search" />
+          </q-item-section>
+          <q-item-section class="text-weight-lg"
+            >Find Therapists</q-item-section
+          >
+        </q-item>
 
-      <q-item
-      to="find"
-      v-ripple
-      clickable
-      exact>
-        <q-item-section avatar>
-          <q-icon name="search" />
-        </q-item-section>
-        <q-item-section class="text-weight-lg">Find Therapists</q-item-section>
-      </q-item>
+        <!-- <q-separator spaced inset="item" /> -->
 
-      <!-- <q-separator spaced inset="item" /> -->
-      
-      <q-item
-      to="account"
-      v-ripple
-      clickable
-      exact>
-        <q-item-section avatar>
-          <q-icon name="account_circle" />
-        </q-item-section>
-        <q-item-section>Account</q-item-section>
-      </q-item>
-      
-    </q-list>
+        <q-item to="account" v-ripple clickable exact>
+          <q-item-section avatar>
+            <q-icon name="account_circle" />
+          </q-item-section>
+          <q-item-section>Account</q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
-<!-- ---------------------------------------------------------------------------------
+    <!-- ---------------------------------------------------------------------------------
 ---------------------------------Right Drawer------------------------------------
 --------------------------------------------------------------------------------- -->
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered :width="250">
-      <q-input color="grey-3" label-color="primary" outlined v-model="text" label="Find" rounded class="q-pa-md" dense>
+    <q-drawer
+      show-if-above
+      v-model="rightDrawerOpen"
+      side="right"
+      bordered
+      :width="250"
+    >
+      <q-input
+        color="grey-3"
+        label-color="primary"
+        outlined
+        v-model="text"
+        label="Find"
+        rounded
+        class="q-pa-md"
+        dense
+      >
         <template v-slot:append>
           <q-icon name="search" color="primary" />
         </template>
       </q-input>
-      
-      
-      <q-list
-        separator
-      >
-        
-        
+
+      <q-list separator>
         <q-item>
           <q-item-section>
-            <q-item-label>Single line item</q-item-label>
-            <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+            <q-item-label lines="1"
+              >Dealing with pandemic stress? You are not alone</q-item-label
+            >
+            <q-item-label caption lines="2"
+              >Studies show that Americans are reporting an increase in mental
+              health issues, as well as substance abuse issues. A Kaiser Family
+              Foundation (KFF) poll said that 53% of Americans
+              reported</q-item-label
+            >
           </q-item-section>
 
           <q-item-section side top>
@@ -97,13 +111,17 @@
             <q-icon name="star" color="yellow" />
           </q-item-section>
         </q-item>
-        
-        
-        
+
         <q-item>
           <q-item-section>
-            <q-item-label>Single line item</q-item-label>
-            <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+            <q-item-label lines="1"
+              >Tips to Manage Anxiety and Stress</q-item-label
+            >
+            <q-item-label caption lines="2"
+              >Take a time-out. Practice yoga, listen to music, meditate, get a
+              massage, or learn relaxation techniques. Stepping back from the
+              problem helps clear your head.</q-item-label
+            >
           </q-item-section>
 
           <q-item-section side top>
@@ -112,35 +130,33 @@
           </q-item-section>
         </q-item>
       </q-list>
-
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const rightDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const rightDrawerOpen = ref(false);
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
       },
 
       rightDrawerOpen,
-      toggleRightDrawer () {
-        rightDrawerOpen.value = !rightDrawerOpen.value
-      }
-    }
-  }
-}
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
+  },
+};
 </script>
--- src/pages/find.vue
+++ src/pages/find.vue
@@ -1,15 +1,39 @@
 <template>
   <q-page class="q-pa-lg">
     <h5 class="q-mt-none q-bm-none">Find</h5>
-    <P>Surprisingly, there is a very vocal faction of the design community that wants to see filler text banished to the original sources from whence it came. Perhaps not surprisingly, in an era of endless quibbling, there is an equally vocal contingent of designers leaping to defend the use of the time-honored tradition of greeking. The argument in favor of using filler text goes something like this: If you use real content in the design process, anytime you reach a review point you’ll end up reviewing and negotiating the content itself and not the design. This will just slow down the design process. Design first, with real content in mind (of course!), but don’t drop in the real content until the design is well on its way. Using filler text avoids the inevitable</P>
-    <P>Surprisingly, there is a very vocal faction of the design community that wants to see filler text banished to the original sources from whence it came. Perhaps not surprisingly, in an era of endless quibbling, there is an equally vocal contingent of designers leaping to defend the use of the time-honored tradition of greeking. The argument in favor of using filler text goes something like this: If you use real content in the design process, anytime you reach a review point you’ll end up reviewing and negotiating the content itself and not the design. This will just slow down the design process. Design first, with real content in mind (of course!), but don’t drop in the real content until the design is well on its way. Using filler text avoids the inevitable</p> 
+    <P
+      >Surprisingly, there is a very vocal faction of the design community that
+      wants to see filler text banished to the original sources from whence it
+      came. Perhaps not surprisingly, in an era of endless quibbling, there is
+      an equally vocal contingent of designers leaping to defend the use of the
+      time-honored tradition of greeking. The argument in favor of using filler
+      text goes something like this: If you use real content in the design
+      process, anytime you reach a review point you’ll end up reviewing and
+      negotiating the content itself and not the design. This will just slow
+      down the design process. Design first, with real content in mind (of
+      course!), but don’t drop in the real content until the design is well on
+      its way. Using filler text avoids the inevitable</P
+    >
+    <P
+      >Surprisingly, there is a very vocal faction of the design community that
+      wants to see filler text banished to the original sources from whence it
+      came. Perhaps not surprisingly, in an era of endless quibbling, there is
+      an equally vocal contingent of designers leaping to defend the use of the
+      time-honored tradition of greeking. The argument in favor of using filler
+      text goes something like this: If you use real content in the design
+      process, anytime you reach a review point you’ll end up reviewing and
+      negotiating the content itself and not the design. This will just slow
+      down the design process. Design first, with real content in mind (of
+      course!), but don’t drop in the real content until the design is well on
+      its way. Using filler text avoids the inevitable</P
+    >
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'find'
-})
+  name: "find",
+});
 </script>
